a14116800e75536108a094c022d75a93d6a16558
Hongbo LU <gitawego@gmail.com>
2014-06-10 09:45:30 +0200

    added patch of issue #10

--- src/ios/Screenshot.m
+++ src/ios/Screenshot.m
@@ -1,12 +1,12 @@
 //
-//  Screenshot.h
+// Screenshot.h
 //
-//  Created by Simon Madine on 29/04/2010.
-//  Copyright 2010 The Angry Robot Zombie Factory.
-//   - Converted to Cordova 1.6.1 by Josemando Sobral.
-//  MIT licensed
+// Created by Simon Madine on 29/04/2010.
+// Copyright 2010 The Angry Robot Zombie Factory.
+// - Converted to Cordova 1.6.1 by Josemando Sobral.
+// MIT licensed
 //
-//  Modifications to support orientation change by @ffd8
+// Modifications to support orientation change by @ffd8
 //
 
 #import <Cordova/CDV.h>
@@ -17,8 +17,15 @@
 @synthesize webView;
 
 //- (void)saveScreenshot:(NSArray*)arguments withDict:(NSDictionary*)options
-- (void)saveScreenshot:(CDVInvokedUrlCommand*)command
+
+(void)saveScreenshot:(CDVInvokedUrlCommand*)command
 {
+	NSString *filename = [command.arguments objectAtIndex:2];
+	NSNumber *quality = [command.arguments objectAtIndex:1];
+	NSString *path = [NSString stringWithFormat:@"%@",filename];
+
+	NSString *jpgPath = [NSTemporaryDirectory() stringByAppendingPathComponent:path ];
+
 	CGRect imageRect;
 	CGRect screenRect = [[UIScreen mainScreen] bounds];
 
@@ -35,10 +42,9 @@
 
 	// Adds support for Retina Display. Code reverts back to original if iOs 4 not detected.
 	if (NULL != UIGraphicsBeginImageContextWithOptions)
-        UIGraphicsBeginImageContextWithOptions(imageRect.size, NO, 0);
-    else
-        UIGraphicsBeginImageContext(imageRect.size);
-
+		UIGraphicsBeginImageContextWithOptions(imageRect.size, NO, 0);
+	else
+		UIGraphicsBeginImageContext(imageRect.size);
 
 	CGContextRef ctx = UIGraphicsGetCurrentContext();
 	[[UIColor blackColor] set];
@@ -47,13 +53,22 @@
 
 	[webView.layer renderInContext:ctx];
 
-	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-	UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+	UIImage image = UIGraphicsGetImageFromCurrentImageContext();
+	NSData imageData = UIImageJPEGRepresentation(image,[quality floatValue]);
+	[imageData writeToFile:jpgPath atomically:NO];
+
 	UIGraphicsEndImageContext();
 
-	UIAlertView *alert= [[UIAlertView alloc] initWithTitle:nil message:@"Image Saved" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-	[alert show];
-	[alert release];
+	CDVPluginResult* pluginResult = nil;
+	NSDictionary *jsonObj = [ [NSDictionary alloc]
+		initWithObjectsAndKeys :
+		jpgPath, @"filePath",
+		@"true", @"success",
+		nil
+		];
+
+	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObj];
+	[self writeJavascript:[pluginResult toSuccessCallbackString:command.callbackId]];
 }
 
 @end
