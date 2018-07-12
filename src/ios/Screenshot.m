98871ca04751295cbb253dc3def8d5af9d129c3e
Anthony Tsang <anthony.tsang@entinux.co.jp>
2018-07-12 13:35:26 +0900

    Fix iOS memory leak when taking screenshot.

--- src/ios/Screenshot.m
+++ src/ios/Screenshot.m
@@ -24,63 +24,68 @@ CGFloat statusBarHeight()
 
 - (UIImage *)getScreenshot
 {
-	UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
-	CGRect rect = [keyWindow bounds];
-	UIGraphicsBeginImageContextWithOptions(rect.size, YES, 0);
-	[keyWindow drawViewHierarchyInRect:keyWindow.bounds afterScreenUpdates:NO];
-	UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
-	UIGraphicsEndImageContext();
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    CGRect rect = [keyWindow bounds];
 
-	// cut the status bar from the screenshot
-	CGRect smallRect = CGRectMake (0,statusBarHeight()*img.scale,rect.size.width*img.scale,rect.size.height*img.scale);
- 
-	CGImageRef subImageRef = CGImageCreateWithImageInRect(img.CGImage, smallRect);
-	CGRect smallBounds = CGRectMake(0,0,CGImageGetWidth(subImageRef), CGImageGetHeight(subImageRef));
+    UIGraphicsBeginImageContextWithOptions(rect.size, YES, 0);
+    [keyWindow drawViewHierarchyInRect:keyWindow.bounds afterScreenUpdates:NO];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
 
-	UIGraphicsBeginImageContext(smallBounds.size);
-	CGContextRef context = UIGraphicsGetCurrentContext();
-	CGContextDrawImage(context,smallBounds,subImageRef);
-	UIImage* cropped = [UIImage imageWithCGImage:subImageRef];
-	UIGraphicsEndImageContext();  
+    // cut the status bar from the screenshot
+    CGRect smallRect = CGRectMake(0,statusBarHeight()*img.scale,rect.size.width*img.scale,rect.size.height*img.scale);
 
-	return cropped;
+    CGImageRef subImageRef = CGImageCreateWithImageInRect(img.CGImage, smallRect);
+    CGRect smallBounds = CGRectMake(0,0,CGImageGetWidth(subImageRef), CGImageGetHeight(subImageRef));
+
+    UIGraphicsBeginImageContext(smallBounds.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextDrawImage(context,smallBounds,subImageRef);
+    UIImage* cropped = [UIImage imageWithCGImage:subImageRef];
+    UIGraphicsEndImageContext();
+
+    CGImageRelease(subImageRef);
+
+    return cropped;
 }
+
+
 
 - (void)saveScreenshot:(CDVInvokedUrlCommand*)command
 {
-	NSString *filename = [command.arguments objectAtIndex:2];
-	NSNumber *quality = [command.arguments objectAtIndex:1];
+  NSString *filename = [command.arguments objectAtIndex:2];
+  NSNumber *quality = [command.arguments objectAtIndex:1];
 
-	NSString *path = [NSString stringWithFormat:@"%@.jpg",filename];
-	NSString *jpgPath = [NSTemporaryDirectory() stringByAppendingPathComponent:path];
+  NSString *path = [NSString stringWithFormat:@"%@.jpg",filename];
+  NSString *jpgPath = [NSTemporaryDirectory() stringByAppendingPathComponent:path];
 
-	UIImage *image = [self getScreenshot];
-	NSData *imageData = UIImageJPEGRepresentation(image,[quality floatValue]);
-	[imageData writeToFile:jpgPath atomically:NO];
+  UIImage *image = [self getScreenshot];
+  NSData *imageData = UIImageJPEGRepresentation(image,[quality floatValue]);
+  [imageData writeToFile:jpgPath atomically:NO];
 
-	CDVPluginResult* pluginResult = nil;
-	NSDictionary *jsonObj = [ [NSDictionary alloc]
-		initWithObjectsAndKeys :
-		jpgPath, @"filePath",
-		@"true", @"success",
-		nil
-	];
+  CDVPluginResult* pluginResult = nil;
+  NSDictionary *jsonObj = [ [NSDictionary alloc]
+    initWithObjectsAndKeys :
+    jpgPath, @"filePath",
+    @"true", @"success",
+    nil
+  ];
 
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObj];
-	NSString* callbackId = command.callbackId;
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObj];
+  NSString* callbackId = command.callbackId;
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
 }
 
 - (void) getScreenshotAsURI:(CDVInvokedUrlCommand*)command
 {
-	NSNumber *quality = command.arguments[0];
-	UIImage *image = [self getScreenshot];
-	NSData *imageData = UIImageJPEGRepresentation(image,[quality floatValue]);
-	NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
-	NSDictionary *jsonObj = @{
-	    @"URI" : [NSString stringWithFormat:@"data:image/jpeg;base64,%@", base64Encoded]
-	};
-	CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObj];
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
+  NSNumber *quality = command.arguments[0];
+  UIImage *image = [self getScreenshot];
+  NSData *imageData = UIImageJPEGRepresentation(image,[quality floatValue]);
+  NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
+  NSDictionary *jsonObj = @{
+      @"URI" : [NSString stringWithFormat:@"data:image/jpeg;base64,%@", base64Encoded]
+  };
+  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObj];
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
 }
 @end
